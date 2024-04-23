e87392a9127cf4f02f57302830bcd6dfcf2e677e
Tran Duy Bim <tranduybim2906@gmail.com>
2024-04-23 17:20:33 +0700

    'full'

---
+++ sieu_thi_mini_bim_bim/sieu_thi_mini/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sieu_thi_mini
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public class MaDangNhapService
+    {
+        private string _maDangNhap;
+
+        public string MaDangNhap
+        {
+            get { return _maDangNhap; }
+            set { _maDangNhap = value; }
+        }
+    }
+
+    public class MatKhauService
+    {
+        private string _matKhau;
+
+        public string MatKhau
+        {
+            get { return _matKhau; }
+            set { _matKhau = value; }
+        }
+    }
+    public partial class App : Application
+    {
+        public static MaDangNhapService MaDangNhapService { get; } = new MaDangNhapService();
+        public static MatKhauService MatKhauService { get; } = new MatKhauService();
+
+        public static string ConnectionString = @"Data Source=LAPTOP-8OU0CA2C\SQLEXPRESS;Initial Catalog=QuanLySieuThi;Integrated Security=True; Connect Timeout=1800";
+
+    }
+}
